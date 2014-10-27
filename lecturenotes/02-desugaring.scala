4e0e13411486ca27e01498a70a91c0e2a04a7020
Klaus Ostermann <klaus612@gmail.com>
2014-10-27 16:12:39 +0100

    lecture notes on desugaring

--- lecturenotes/02-desugaring.scala
+++ lecturenotes/02-desugaring.scala
@@ -11,21 +11,21 @@ propose corrections as a github pull request.
 */
 
 /* In the exercises you have already seen the basic structure of an interpreter
- * by means of an interpreter for a language of arithmetic expressions: */
+ * by means of an interpreter for a language of arithmetic Exps: */
 object AE {
   // Abstract Syntax Tree
-  sealed trait Expression // we use "sealed" to get completeness checks for pattern matching
-  case class Literal(n: Int) extends Expression
-  case class Addition(lhs: Expression, rhs: Expression) extends Expression
+  sealed trait Exp // we use "sealed" to get completeness checks for pattern matching
+  case class Num(n: Int) extends Exp
+  case class Add(lhs: Exp, rhs: Exp) extends Exp
 
   // Example
-  val ex = Addition(Literal(1), Addition(Literal(5), Literal(3)))
+  val ex = Add(Num(1), Add(Num(5), Num(3)))
 
   // Interpreter
-  def eval(e: Expression): Int =
+  def eval(e: Exp): Int =
     e match {
-      case Literal(n) => n
-      case Addition(lhs, rhs) =>
+      case Num(n) => n
+      case Add(lhs, rhs) =>
         eval(lhs) + eval(rhs)
     }
 }
@@ -43,25 +43,25 @@ object AE {
  * Extension number 1 is a good example for a core language extension.
  * We have no way of expressing multiplication in terms of the existing 
  * constructs (if we had some looping construct we could express multiplication
- * as repeated addition but we do not have loops).
+ * as repeated Add but we do not have loops).
  * 
  * Hence we add this language construct to the (core) language:
  */
 
 object MAE {
   // Abstract Syntax Tree
-  sealed trait Expression
-  case class Literal(n: Int) extends Expression
-  case class Addition(lhs: Expression, rhs: Expression) extends Expression
-  case class Multiplication(lhs: Expression, rhs: Expression) extends Expression
+  sealed trait Exp
+  case class Num(n: Int) extends Exp
+  case class Add(lhs: Exp, rhs: Exp) extends Exp
+  case class Multiplication(lhs: Exp, rhs: Exp) extends Exp
   // Example
-  val ex = Addition(Literal(1), Multiplication(Literal(5), Literal(3)))
+  val ex = Add(Num(1), Multiplication(Num(5), Num(3)))
 
   // Interpreter
-  def eval(e: Expression): Int =
+  def eval(e: Exp): Int =
     e match {
-      case Literal(n) => n
-      case Addition(lhs, rhs) => eval(lhs) + eval(rhs)
+      case Num(n) => n
+      case Add(lhs, rhs) => eval(lhs) + eval(rhs)
       case Multiplication(lhs, rhs) =>  eval(lhs) * eval(rhs)
     }
 }
@@ -72,19 +72,19 @@ object MAE {
 
 object SMAE {
   // Abstract Syntax Tree
-  sealed trait Expression
-  case class Literal(n: Int) extends Expression
-  case class Addition(lhs: Expression, rhs: Expression) extends Expression
-  case class Multiplication(lhs: Expression, rhs: Expression) extends Expression
-  case class Subtraction(lhs: Expression, rhs: Expression) extends Expression
+  sealed trait Exp
+  case class Num(n: Int) extends Exp
+  case class Add(lhs: Exp, rhs: Exp) extends Exp
+  case class Multiplication(lhs: Exp, rhs: Exp) extends Exp
+  case class Subtraction(lhs: Exp, rhs: Exp) extends Exp
   // Example
-  val ex = Subtraction(Literal(1), Multiplication(Literal(5), Literal(3)))
+  val ex = Subtraction(Num(1), Multiplication(Num(5), Num(3)))
 
   // Interpreter
-  def eval(e: Expression): Int =
+  def eval(e: Exp): Int =
     e match {
-      case Literal(n) => n
-      case Addition(lhs, rhs) => eval(lhs) + eval(rhs)
+      case Num(n) => n
+      case Add(lhs, rhs) => eval(lhs) + eval(rhs)
       case Multiplication(lhs, rhs) =>  eval(lhs) * eval(rhs)
       case Subtraction(lhs, rhs) =>  eval(lhs) - eval(rhs)
     }
@@ -95,13 +95,13 @@ object SMAE {
  *
  * One way of expressing the desugaring is as a syntax transformation: */
 
-def desugarSMAE2MAE(e: SMAE.Expression) : MAE.Expression = e match {
-  case SMAE.Literal(n) => MAE.Literal(n)
-  case SMAE.Addition(lhs, rhs) => MAE.Addition(desugarSMAE2MAE(lhs), desugarSMAE2MAE(rhs))
+def desugarSMAE2MAE(e: SMAE.Exp) : MAE.Exp = e match {
+  case SMAE.Num(n) => MAE.Num(n)
+  case SMAE.Add(lhs, rhs) => MAE.Add(desugarSMAE2MAE(lhs), desugarSMAE2MAE(rhs))
   case SMAE.Multiplication(lhs, rhs) => MAE.Multiplication(desugarSMAE2MAE(lhs), desugarSMAE2MAE(rhs)) 
   case SMAE.Subtraction(lhs, rhs) => 
-    MAE.Addition(desugarSMAE2MAE(lhs), 
-                 MAE.Multiplication(MAE.Literal(-1),desugarSMAE2MAE(rhs)))
+    MAE.Add(desugarSMAE2MAE(lhs), 
+                 MAE.Multiplication(MAE.Num(-1),desugarSMAE2MAE(rhs)))
 }
 
 /* With this desugaring in place, we do not need an interpreter for SMAE anymore; rather
@@ -114,7 +114,7 @@ val res = MAE.eval(desugarSMAE2MAE(SMAE.ex))
  * It is important, though, that the desugared language feature is gone after desugaring.
  * For instance, a pretty printer would print the desugared code. A debugger would use
  * the desugared code. This can be an important downside to desugaring. There are ways
- * to avoid or mitigate these shortcomings, but they require additional work.
+ * to avoid or mitigate these shortcomings, but they require Addal work.
  *
  * There is a second way of realizing desugaring which does not require the definition of
  * a copy of the AST classes. We can desugar earlier, namely during the construction of the AST:
@@ -122,22 +122,22 @@ val res = MAE.eval(desugarSMAE2MAE(SMAE.ex))
 
 object SMAE2 {
   // Abstract Syntax Tree
-  sealed trait Expression
-  case class Literal(n: Int) extends Expression
-  case class Addition(lhs: Expression, rhs: Expression) extends Expression
-  case class Multiplication(lhs: Expression, rhs: Expression) extends Expression
-  def subtraction(e1: Expression, e2: Expression) : Expression =
-    Addition(e1, Multiplication(Literal(-1), e2))
+  sealed trait Exp
+  case class Num(n: Int) extends Exp
+  case class Add(lhs: Exp, rhs: Exp) extends Exp
+  case class Multiplication(lhs: Exp, rhs: Exp) extends Exp
+  def subtraction(e1: Exp, e2: Exp) : Exp =
+    Add(e1, Multiplication(Num(-1), e2))
   
   // Compared to SMAE, we only have to change upper case Subtraction by lower case subtraction
   // when constructing examples.
-  val ex = subtraction(Literal(1), Multiplication(Literal(5), Literal(3)))
+  val ex = subtraction(Num(1), Multiplication(Num(5), Num(3)))
 
   // Interpreter - no case for subtraction needed
-  def eval(e: Expression): Int =
+  def eval(e: Exp): Int =
     e match {
-      case Literal(n) => n
-      case Addition(lhs, rhs) => eval(lhs) + eval(rhs)
+      case Num(n) => n
+      case Add(lhs, rhs) => eval(lhs) + eval(rhs)
       case Multiplication(lhs, rhs) =>  eval(lhs) * eval(rhs)
     }
 }
@@ -153,23 +153,23 @@ object SMAE2 {
 
 object USMAE {
   // Abstract Syntax Tree
-  sealed trait Expression
-  case class Literal(n: Int) extends Expression
-  case class Addition(lhs: Expression, rhs: Expression) extends Expression
-  case class Multiplication(lhs: Expression, rhs: Expression) extends Expression
-  def subtraction(e1: Expression, e2: Expression) : Expression =
-    Addition(e1, Multiplication(Literal(-1), e2))
-  def unaryminus(e: Expression) = subtraction(Literal(0), e)
+  sealed trait Exp
+  case class Num(n: Int) extends Exp
+  case class Add(lhs: Exp, rhs: Exp) extends Exp
+  case class Multiplication(lhs: Exp, rhs: Exp) extends Exp
+  def subtraction(e1: Exp, e2: Exp) : Exp =
+    Add(e1, Multiplication(Num(-1), e2))
+  def unaryminus(e: Exp) = subtraction(Num(0), e)
   
   // Compared to SMAE, we only have to change upper case Subtraction by lower case subtraction
   // when constructing examples.
-  val ex = subtraction(unaryminus(Literal(1)), Multiplication(Literal(5), Literal(3)))
+  val ex = subtraction(unaryminus(Num(1)), Multiplication(Num(5), Num(3)))
 
   // Interpreter - no case for subtraction needed
-  def eval(e: Expression): Int =
+  def eval(e: Exp): Int =
     e match {
-      case Literal(n) => n
-      case Addition(lhs, rhs) => eval(lhs) + eval(rhs)
+      case Num(n) => n
+      case Add(lhs, rhs) => eval(lhs) + eval(rhs)
       case Multiplication(lhs, rhs) =>  eval(lhs) * eval(rhs)
     }
 }
